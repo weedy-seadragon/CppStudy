17bbb19732419551f9ed559cae3d1b592fede7b3
weedy-seadragon <211094980+weedy-seadragon@users.noreply.github.com>
2026-07-02 18:36:52 +0900

    コンソールに日本語を表示

---
+++ CppBasis/HelloWorld2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+int main() {
+#ifdef _WIN32
+	//コンソール出力をUTF-8として扱う（文字化け対策）
+	SetConsoleOutputCP(CP_UTF8);
+#endif
+	//コンソールにHelloWorldを出力
+	std::cout << "い゛や゛あ゛あ゛あ゛あ゛あ゛あ゛あ゛あ゛あ゛！！！！！ミヤコを消さないでな゛の゛お゛お゛お゛お゛お゛お゛お゛お゛！！！！！逝゛く゛っ゛！！逝゛っ゛ぢゃ゛う゛な゛の゛お゛お゛お゛お゛お゛！！！！！成仏する！！成仏アクメするな゛の゛お゛お゛お゛お゛お゛お゛お゛お゛！！！！！！" << std::endl;
+}
